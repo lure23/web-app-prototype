eb7ea2a2a336b544dde28d848e2ae318f57e4c88
Asko Kauppi <demo@outstanding.earth>
2023-08-08 22:22:29 +0300

    minute

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -5,6 +5,10 @@
 //    https://vercel.com/docs/concepts/projects/environment-variables/system-environment-variables#system-environment-variables
 //
 
+// Note: 
+//  Could use 'git status --porcelain' (or 'git status -s'). Or make so that only when run via CI/CD, the SHA would be shown
+//  (then we know it's a read-only clone).
+
 import { VERCEL_GIT_COMMIT_SHA } from '$env/static/private';
 //const VERCEL_GIT_COMMIT_SHA = process.env['VERCEL_GIT_COMMIT_SHA'];    // undefined | <string>
 
